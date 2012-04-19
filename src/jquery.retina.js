94d0cf30390dd0408720dc4af4f5e568a6497eed
Casey O'Hara <casey.ohara@imulus.com>
2012-04-19 02:52:47 -0600

    Add safety check to ensure we only pass image elements to RetinaImage

--- src/jquery.retina.js
+++ src/jquery.retina.js
@@ -7,7 +7,7 @@
     return this.each(function() {        
       var $this = $(this);
       var image = $this.get(0);      
-      if (window.devicePixelRatio > 1) {
+      if (window.devicePixelRatio > 1 && image.tagName == 'IMG') {
         new RetinaImage(image);
       }
     });
